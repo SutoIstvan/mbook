1534a6c45a853c344f103b8cfd0dc2cb73e821b2
SutoIstvan <isutoi@gmail.com>
2025-03-20 00:50:46 +0200

    update

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -11,7 +11,79 @@
         </div>
 
         <div class="">
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    <label for="slug" class="col-form-label text-md-end">{{ __('Slug') }}</label>
+                    <input id="slug" type="text" class="form-control @error('slug') is-invalid @enderror" name="slug" value="{{ old('slug', $memorial->slug) }}" required>
+                    @error('slug')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    <label class="col-form-label text-md-end">{{ __('Private') }}</label>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="private" name="private" {{ old('private', $memorial->private) ? 'checked' : '' }}>
+                        <label class="form-check-label" for="private">{{ __('Make this private') }}</label>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    <label for="map_address" class="col-form-label text-md-end">{{ __('Address on Map') }}</label>
+                    <input id="map_address" type="text" class="form-control @error('map_address') is-invalid @enderror" name="map_address" value="{{ old('map_address', $memorial->map_address) }}">
+                    @error('map_address')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
 
+
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    <label class="col-form-label text-md-end">{{ __('Theme') }}</label>
+                    <div class="d-flex justify-content-between mt-2">
+                        <!-- Светлая тема -->
+                        <div class="col-5">
+                            <label class="theme-option">
+                                <img src="{{ asset('light.png') }}" style="border-radius: 15px" alt="Light Theme" class="theme-thumbnail">
+                                <span class="mt-2">
+                                    <input type="radio" name="theme" value="light" id="lightTheme">
+                                    {{ __('Light Theme') }}
+                                </span>
+                            </label>
+                        </div>
+
+                        <div class="col-5">
+                            <label class="theme-option">
+                                <img src="{{ asset('dark.png') }}" style="border-radius: 15px" alt="Dark Theme" class="theme-thumbnail">
+                                <span class="mt-2">
+                                    <input type="radio" name="theme" value="dark" id="darkTheme">
+                                    {{ __('Dark Theme') }}
+                                </span>
+                            </label>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row mb-3 mt-50">
+                <div class="col-md-12">
+                    <form action="" method="POST" onsubmit="return confirm('{{ __('Are you sure you want to delete this page?') }}');">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger w-100">{{ __('Delete Page') }}</button>
+                    </form>
+                </div>
+            </div>
         </div>
 
 
