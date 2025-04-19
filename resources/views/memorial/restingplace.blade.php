6e624c2ee1f44cd624cc988b53cbef5c00a2b8ba
SutoIstvan <isutoi@gmail.com>
2025-04-19 13:20:17 +0300

    update

--- resources/views/memorial/restingplace.blade.php
+++ resources/views/memorial/restingplace.blade.php
@@ -911,6 +911,23 @@
                                 <input type="text" class="form-control" id="grave_location" name="grave_location"
                                     placeholder="Pl. Kossuth Lajos utca 1, Budapest" value="{{ $memorial->grave_location }}" required>
                             </div>
+
+                            <div class="mb-3">
+                                <label for="grave_parcel" class="form-label">Temető címe</label>
+                                <input type="text" class="form-control" id="grave_parcel" name="grave_parcel"
+                                    placeholder="Pl. Kossuth Lajos utca 1, Budapest" value="{{ $memorial->grave_parcel }}" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="grave_line" class="form-label">Temető címe</label>
+                                <input type="text" class="form-control" id="grave_line" name="grave_line"
+                                    placeholder="Pl. Kossuth Lajos utca 1, Budapest" value="{{ $memorial->grave_line }}" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="grave_number" class="form-label">Temető címe</label>
+                                <input type="text" class="form-control" id="grave_number" name="grave_number"
+                                    placeholder="Pl. Kossuth Lajos utca 1, Budapest" value="{{ $memorial->grave_number }}" required>
+                            </div>
+
                             <div class="row">
                                 <div class="col-md-12 mb-3">
                                     <label for="coordinates" class="form-label">Coordinates</label>
@@ -959,6 +976,7 @@
         // 'establishment' подходит для разных учреждений и мест, включая кладбища
         const options = {
             types: ['establishment'], 
+            language: '{{ $locale }}',
             // Можно добавить ограничение по стране, если нужно
             // componentRestrictions: {country: 'ru'}
         };
